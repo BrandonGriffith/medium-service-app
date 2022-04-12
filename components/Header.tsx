4a529d37317f608861c129f10e995e07a8da3a9e
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-12 16:51:45 -0400

    added header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link"
+
 const Header = () => {
     return (
-    <header>
-        <div></div>
-        <div></div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+        <div className="flex items-center space-x-5">
+            <Link href="/">
+                <img src="/logo512.png" alt="" className="w-[10%] object-contain cursor-pointer" />
+            </Link>
+            <div className="hidden md:inline-flex items-center space-x-5">
+                <h3 className="bg-orange-700 px-4 py-1 rounded-full font-bold cursor-pointer">About</h3>
+                <h3 className="bg-blue-700 px-4 py-1 rounded-full font-bold cursor-pointer">Contact</h3>
+                <h3 className="bg-green-700 px-4 py-1 rounded-full font-bold cursor-pointer">Follow</h3>
+            </div>
+        </div>
+
+        <div className="flex items-center space-x-11 whitespace-nowrap">
+            <h3 className="cursor-pointer font-bold">
+                Sign In
+            </h3>
+            <h3 className="cursor-pointer font-bold">
+                Get Started
+            </h3>
+        </div>
     </header>
     )
 }
