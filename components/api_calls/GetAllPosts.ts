3f950f2e919441ced74ef4a340a8cc20b87fad46
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-14 19:50:37 -0400

    added env

--- components/api_calls/GetAllPosts.ts
+++ components/api_calls/GetAllPosts.ts
@@ -1,7 +1,8 @@
 const GetAllPosts = async () => {
     console.log("<GetAllPosts> -- this is an api call running on the server.")
+    const server = process.env.SERVER
     const result = await fetch(
-        `http://localhost:3000/api`
+        `http://${server}/api`
         ).then( res => res.json() )
     return result
 }
